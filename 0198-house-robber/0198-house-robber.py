ed942d186ee146774645192c4de15b1e8a75c2b8
sakshidalmiya02 <131193340+sakshidalmiya02@users.noreply.github.com>
2023-05-23 11:35:52 +0530

    Time: 48 ms (24.87%), Space: 16.3 MB (11.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        yes=[0]*(n+1)
-        no=[0]*(n+1)
+        yes,no=0,0
         for i in range(len(nums)):
-            yes[i+1]=nums[i]+no[i]
-            no[i+1]=max(no[i],yes[i])
-        return max(yes[-1],no[-1])
+            temp=max(yes,no)
+            yes=no+nums[i]
+            no=temp
+        return max(yes,no)
