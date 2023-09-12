4f4410b80a23c27bfa45e060f29a8c192f73b678
sakshidalmiya02 <131193340+sakshidalmiya02@users.noreply.github.com>
2023-09-13 00:29:08 +0530

    Time: 42 ms (36.15%), Space: 16.3 MB (41.89%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,16 +1,13 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        dp=[0]*(n+1)
+        
         if n==1:
             return 1
         if n==2:
             return 6
-        dp[1]=1
-    
-        dp[2]=6
-        
+        prev=6
         for i in range(3,n+1):
-            
-            dp[i]=(i)*dp[i-1]*(2*(i-1)+1)
-        return dp[-1]%(10**9 + 7)
+            ans=i*prev*(2*(i-1)+1)
+            prev=ans
+        return ans%(10**9 + 7)
         
